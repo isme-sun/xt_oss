e90fead45e362e7d1f1b9dc2b4752e3e4f716153
树熊 <150658376+isme-sun@users.noreply.github.com>
2024-02-01 13:56:49 +0800

    测试

--- src/utils.rs
+++ src/utils.rs
@@ -1,42 +1,26 @@
-use std::env;
-
 use crate::oss;
+
+pub fn get_env(key: &str, default: &str) -> String {
+  std::env::var(key).unwrap_or_else(|_| default.to_owned())
+}
+
+pub fn get_env_bool(key: &str, default: bool) -> bool {
+  std::env::var(key)
+    .unwrap_or(default.to_string())
+    .parse()
+    .unwrap_or(default)
+}
 
 pub fn options_from_env() -> oss::Options<'static> {
   oss::Options::new()
-    .with_access_key_id(env::var("OSS_ACCESS_KEY_ID").unwrap_or_default().leak())
-    .with_access_key_secret(env::var("OSS_ACCESS_KEY_SECRET").unwrap_or_default().leak())
-    .with_region(env::var("OSS_REGION").unwrap_or_default().leak())
-    .with_bucket(env::var("OSS_BUCKET").unwrap_or_default().leak())
-    .with_sts_token(env::var("OSS_STS_TOKEN").unwrap_or_default().leak())
-    .with_internal(
-      env::var("OSS_INTERNAL")
-        .unwrap_or_default()
-        .parse::<bool>()
-        .unwrap_or(false),
-    )
-    .with_cname(
-      env::var("OSS_CNAME")
-        .unwrap_or_default()
-        .parse::<bool>()
-        .unwrap_or(false),
-    )
-    .with_is_request_pay(
-      env::var("OSS_IS_REQUEST_PAY")
-        .unwrap_or_default()
-        .parse::<bool>()
-        .unwrap_or(false),
-    )
-    .with_secret(
-      env::var("OSS_SECURE")
-        .unwrap_or_default()
-        .parse::<bool>()
-        .unwrap_or(false),
-    )
-    .with_timeout(
-      env::var("OSS_TIMEOUT")
-        .unwrap_or(oss::DEFAULT_TIMEOUT.to_string())
-        .parse::<u64>()
-        .unwrap_or(60),
-    )
+    .with_access_key_id(get_env("OSS_ACCESS_KEY_ID", "").leak())
+    .with_access_key_secret(get_env("OSS_ACCESS_KEY_SECRET", "").leak())
+    .with_region(get_env("OSS_REGION", "").leak())
+    .with_bucket(get_env("OSS_BUCKET", "").leak())
+    .with_sts_token(get_env("OSS_STS_TOKEN", "").leak())
+    .with_internal(get_env_bool("OSS_INTERNAL", false))
+    .with_cname(get_env_bool("OSS_CNAME", false))
+    .with_is_request_pay(get_env_bool("OSS_IS_REQUEST_PAY", false))
+    .with_secret(get_env_bool("OSS_SECURE", false))
+    .with_timeout(get_env("OSS_TIMEOUT", "60").parse().unwrap())
 }
